d0903e81e7fd58e5224b698f078206abb73a2dc2
Hamza.Noah <hamza.nouh97@gmail.com>
2023-11-28 20:49:02 +0200

    added the project for practicing react

---
+++ src/Header/Header.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function Header() {
+  const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+  const randomReactDescription = (max) => {
+    return Math.floor(Math.random() * (max + 1));
+  };
+
+  const description = reactDescriptions[randomReactDescription(2)]
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any
+        app you are going to build!
+      </p>
+    </header>
+  );
+}
+
+export default Header;
