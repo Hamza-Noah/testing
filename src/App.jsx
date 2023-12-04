7d638e3a0817696d4344fbe2a398e573d3ccffad
Hamza.Noah <hamza.nouh97@gmail.com>
2023-12-04 13:39:58 +0200

    added the tabs to switch the content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import CoreConcepts from "./components/Core-Concept/index.jsx";
 import Header from "./components/Header/index.jsx";
-// import ComponentImg from "./assets/components.png";
+import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data.js";
 
 function App() {
@@ -21,6 +21,13 @@ function App() {
   //   console.log(pizza);
   // };
 
+  const [selecteTopic, setSelecteTopic] = useState("Please select a topic");
+
+  function handleClick(selectedButton) {
+    setSelecteTopic(selectedButton);  
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       {/* <button onClick={handleClick}>this</button> */}
@@ -30,9 +37,44 @@ function App() {
           <h2>Core Concpets</h2>
           <ul>
             {CORE_CONCEPTS.map((core_concept) => {
-              return <CoreConcepts {...core_concept} />;
+              return (
+                <CoreConcepts key={core_concept.title} {...core_concept} />
+              );
             })}
           </ul>
+        </section>
+        <section id="examples">
+          <menu>
+            <TabButton
+              onSelect={(_) => {
+                handleClick("Components");
+              }}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              onSelect={(_) => {
+                handleClick("JSX");
+              }}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              onSelect={(_) => {
+                handleClick("Props");
+              }}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              onSelect={(_) => {
+                handleClick("State");
+              }}
+            >
+              State
+            </TabButton>
+          </menu>
+          <p>{selecteTopic}</p>
         </section>
       </main>
     </div>
